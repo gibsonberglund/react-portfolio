220559d08e0b70cee55f1558dbf347b77351f3a9
Your Name <berglundgibson@gmail.com>
2023-03-25 15:24:22 -0300

    first piece of display working

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import PortfolioItems from './PortfolioItems';
+
+function DisplayPortfolio() {
+return (
+    <div>
+        <h1>Gibson Berglund's Web Dev Portfolio</h1>
+        <PortfolioItems />
+    </div>
+)
+};
+
+export default DisplayPortfolio;
