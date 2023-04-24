cda376d500a340b1505995c2068cee882c906ae6
Your Name <berglundgibson@gmail.com>
2023-04-24 11:17:52 -0400

    styling changes, new resume link

--- src/components/Pages/Resume.js
+++ src/components/Pages/Resume.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Resume() {
     return (
 <div>
-    <a className='resumelink' href='../../assets/Gibson-Berglund-Resume-PDF.pdf' download>Click here to download a copy of my resume</a>
+    <a className='resumelink' href='https://docs.google.com/document/d/1u4NL1CkakeYskCK3FQ1M3_MGEXl0VH6jr3AIgwtqLqQ/edit?usp=sharing'>Click here to view or download my resume</a>
 </div>
     );
 };
