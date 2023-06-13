10d8c31b78ea0823b73c56f1f1fafd78bf55ebc3
Your Name <berglundgibson@gmail.com>
2023-06-13 13:52:52 -0400

    added email to resume page

--- src/components/Pages/ContactMe.js
+++ src/components/Pages/ContactMe.js
@@ -34,6 +34,10 @@ function ContactMe() {
             <div className='resumebtn'>
                 <Button href={ResumePDF} download={ResumePDF} style={{width: '150px', textAlign: 'center', backgroundColor: 'rgba(125, 125, 125, 0.4)'}}variant="contained"   >Download My Resume<img className='icon' src={pdfIcon}></img></Button>
             </div>
+            <div style={{marginLeft: '20%'}}>
+                <h2>Email Me: </h2>
+                <p>berglundgibson@gmail.com</p>
+            </div>
             {/* <div className='resumebtn'>
                 
                 <Button variant="contained" href="https://www.linkedin.com/in/gibson-berglund/" color="primary"><img className='icon' src={LinkedInIcon}></img>Find me on LinkedIn</Button>
